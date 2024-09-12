dea108fc848e36735dc0a18b329cf04dd1968b45
ChoiKiWon <longvaca0213@gmail.com>
2024-09-12 16:56:13 +0900

    페이징까지...

--- src/main/java/com/board/board/controller/BoardController.java
+++ src/main/java/com/board/board/controller/BoardController.java
@@ -3,8 +3,14 @@ package com.board.board.controller;
 import com.board.board.dto.BoardDto;
 import com.board.board.service.BoardService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/board")
@@ -24,4 +30,58 @@ public class BoardController {
         boardService.save(boardDto);
         return "index";
     }
+
+    @GetMapping("/")
+    public String findAll(Model model) {
+        List<BoardDto> boardDtoList = boardService.findAll();
+        model.addAttribute("boardList", boardDtoList);
+        return "list";
+    }
+
+    @GetMapping("/{id}")
+    public String findById(@PathVariable Long id, Model model) {
+        boardService.updateHits(id);
+        BoardDto boardDto = boardService.findById(id);
+        model.addAttribute("board", boardDto);
+        return "detail";
+    }
+
+    @GetMapping("/update/{id}")
+    public String updateForm(@PathVariable Long id, Model model) {
+        BoardDto boardDto = boardService.findById(id);
+        model.addAttribute("boardUpdate", boardDto);
+        return "update";
+    }
+
+    @PostMapping("/update")
+    public String update(@ModelAttribute BoardDto boardDto, Model model) {
+        BoardDto board = boardService.update(boardDto);
+        model.addAttribute("board", board);
+        return "detail";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable Long id) {
+        boardService.delete(id);
+        return "redirect:/board/";
+    }
+
+    @GetMapping("/paging")
+    public String paging(@PageableDefault(page = 1) Pageable pageable, Model model) {
+//        pageable.getPageNumber();
+        Page<BoardDto> boardList = boardService.paging(pageable);
+
+        int blockLimit = 3;
+
+        int startPage = (((int)(Math.ceil((double)pageable.getPageNumber() / blockLimit))) - 1) * blockLimit + 1; // 1 4 7 10 ~~
+        int endPage = ((startPage + blockLimit - 1) < boardList.getTotalPages()) ? startPage + blockLimit - 1 : boardList.getTotalPages();
+
+        model.addAttribute("boardList", boardList);
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
+
+        return "paging";
+    }
+
+
 }
--- src/main/java/com/board/board/service/BoardService.java
+++ src/main/java/com/board/board/service/BoardService.java
@@ -4,7 +4,16 @@ import com.board.board.dto.BoardDto;
 import com.board.board.entity.BoardEntity;
 import com.board.board.repository.BoardRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Stream;
 
 @Service
 @RequiredArgsConstructor
@@ -15,5 +24,64 @@ public class BoardService {
     public void save(BoardDto boardDto) {
         BoardEntity boardEntity = BoardEntity.toSaveEntity(boardDto);
         boardRepository.save(boardEntity);
+    }
+
+    public List<BoardDto> findAll() {
+        List<BoardEntity> boardEntityList = boardRepository.findAll();
+        List<BoardDto> boardDtoList = boardEntityList
+                .stream()
+                .map(b -> new BoardDto(b.getId(), b.getBoardWriter(), b.getBoardPass(), b.getBoardTitle()
+                        , b.getBoardContents(), b.getBoardHits(), b.getCreatedTime(), b.getUpdatedTime())).toList();
+        return boardDtoList;
+    }
+
+    @Transactional
+    public void updateHits(Long id) {
+        boardRepository.updateHits(id);
+    }
+
+    public BoardDto findById(Long id) {
+        Optional<BoardEntity> optionalBoardEntity = boardRepository.findById(id);
+        if (optionalBoardEntity.isPresent()) {
+            BoardEntity boardEntity = optionalBoardEntity.get();
+            BoardDto boardDto = new BoardDto(boardEntity.getId(), boardEntity.getBoardWriter(), boardEntity.getBoardPass()
+                    , boardEntity.getBoardTitle(), boardEntity.getBoardContents(), boardEntity.getBoardHits(), boardEntity.getCreatedTime(), boardEntity.getUpdatedTime());
+            return boardDto;
+        }
+        return null;
+    }
+
+    @Transactional
+    public BoardDto update(BoardDto boardDto) {
+        Optional<BoardEntity> optionalBoardEntity = boardRepository.findById(boardDto.getId());
+        if (optionalBoardEntity.isPresent()) {
+            BoardEntity boardEntity = optionalBoardEntity.get();
+            boardEntity.setBoardWriter(boardDto.getBoardWriter());
+            boardEntity.setBoardPass(boardDto.getBoardPass());
+            boardEntity.setBoardTitle(boardDto.getBoardTitle());
+            boardEntity.setBoardContents(boardDto.getBoardContents());
+            boardEntity.setBoardHits(boardDto.getBoardHits());
+
+            BoardDto updatedDto = new BoardDto(boardEntity.getId(), boardEntity.getBoardWriter(), boardEntity.getBoardPass()
+                    , boardEntity.getBoardTitle(), boardEntity.getBoardContents(), boardEntity.getBoardHits(), boardEntity.getCreatedTime(), boardEntity.getUpdatedTime());
+            return updatedDto;
+        }
+        return null;
+    }
+
+    public void delete(Long id) {
+        boardRepository.deleteById(id);
+    }
+
+    public Page<BoardDto> paging(Pageable pageable) {
+        int page = pageable.getPageNumber() - 1;
+        int pageLimit = 3; //한 페이지에 보여줄 글 갯수
+
+        Page<BoardEntity> boardEntities = boardRepository.findAll(PageRequest.of(page, pageLimit, Sort.by(Sort.Direction.DESC,"id")));
+
+        Page<BoardDto> boardDtos = boardEntities.map(BoardEntity -> new BoardDto(BoardEntity.getId(), BoardEntity.getBoardWriter(), BoardEntity.getBoardPass(), BoardEntity.getBoardTitle()
+                , BoardEntity.getBoardContents(), BoardEntity.getBoardHits(), BoardEntity.getCreatedTime(), BoardEntity.getUpdatedTime()));
+
+        return boardDtos;
     }
 }
